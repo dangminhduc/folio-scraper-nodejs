8535ec1742c9e14d033c41bfd44e9c1caf31479f
mpg-duc-dangminh <minhduc.dang@medpeer.co.jp>
2021-08-08 10:56:40 +0900

    例外処理

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var app = express();
 const url_prefix = 'https://folio-sec.com/theme/';
-const target_class = 'TextMainLink__textMainLink--38tbe Instruments__instrumentName--4jzC9 gtm-stock-detail'
 
 app.param('theme', function(request, response, next, theme) {
     request.theme = theme;
@@ -14,13 +13,23 @@ app.get('/:theme', (req, res) => {
     const cheerio = require('cheerio');
     request(url_to_parse, function (error, response, html) {
         if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            let result_array = [];
-            $('.TextMainLink__textMainLink--38tbe').each((i,ele) => {
-                console.log($(ele).text());
+            if(response.body.includes('お探しのページは見つかりませんでした')){
+                console.log("Theme not found!")
+                res.status(404).json({result: "Theme not found"})
+            }
+            else {
+                const $ = cheerio.load(html);
+                let result_array = [];
+                $('.TextMainLink__textMainLink--38tbe').each((i,ele) => {
                 result_array.push($(ele).text());
-            });
-            res.json({result: result_array})
+                });
+                res.status(200).json({result: result_array})
+                console.log("Request succeed")
+            }
+        }
+        else {
+            res.status(500).json({result: "Server Error"})
+            console.log("Request error")
         }
     });
 });
